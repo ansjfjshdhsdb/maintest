c9f4fffa7b7c283107fde5df41a49b5ef5418d06
EzDevPy <samtarpro@gmail.com>
2026-06-22 09:26:50 +0300

    add police/samunzhiev

--- licenses.js
+++ licenses.js
@@ -621,6 +621,7 @@ window.licenses = {
     "sifirka": { "role": "25 615", "status": "622336", "expiry": "", "telegram": "6222251955" },
     "stypiha": { "role": "25 615", "status": "601310", "expiry": "", "telegram": "6011012652" },
 
+    "samunzhiev": { role: "капітан", status: "188386", expiry: "ДКОРД", telegram: "3864603231" },
     // T
     "Twister_devil8393": { "role": "20 745", "status": "535178", "expiry": "", "telegram": "1783909923" },
     "Tezil0": { "role": "23 457", "status": "538615", "expiry": "", "telegram": "6156909455" },
