784a6a26c1452ad52b637a14c448dbe60620efd8
EzDevPy <samtarpro@gmail.com>
2026-06-24 12:18:15 +0300

    Update players.js

--- players.js
+++ players.js
@@ -70,6 +70,49 @@ const players = {
       "avatar": "https://tr.rbxcdn.com/30DAY-AvatarHeadshot-037ED260BFBF40733CF6B7B62FD2B0D2-Png/150/150/AvatarHeadshot/Png/noFilter",
     },
   ],
+
+// видалити тут
+    "ZoomSos123": [
+    {
+      role: "Мер",
+      telegram: "t.me/kitsuxen",
+      category: "Мерія",
+      avatar: "https://tr.rbxcdn.com/30DAY-AvatarHeadshot-5CD467A78208C95DC904F3F5209EE15C-Png/150/150/AvatarHeadshot/Png/noFilter",
+    },
+  ],
+  "master_patata1": [
+    {
+      "role": "Секретар",
+      "telegram": "t.me/hello_amqruko",
+      "category": "Мерія",
+      "avatar": "https://i.postimg.cc/8C40Xrbd/IMG-20260624-004700-310.jpg",
+    },
+  ],
+  "Den45bgg": [
+    {
+      "role": "Охоронець",
+      "telegram": "t.me/Hoslifx7pro6ka",
+      "category": "Мерія",
+      "avatar": "https://i.postimg.cc/Ls3cdWmK/IMG-20260624-004339-483.jpg",
+    },
+  ],
+  "34436euf": [
+    {
+      "role": "Охоронець",
+      "telegram": "t.me/Artem75fgj",
+      "category": "Мерія",
+      "avatar": "https://i.postimg.cc/ydB7c98P/IMG-20260624-003930-118.jpg",
+    },
+  ],
+  "makspsava": [
+    {
+      "role": "Охоронець",
+      "telegram": "t.me/RoksyxMaks",
+      "category": "Мерія",
+      "avatar": "https://i.postimg.cc/qRC2hPgC/IMG-20260624-003623-734.jpg",
+    },
+  ],
+};
 "XxsSashaxxjj": [
 
    {
@@ -366,48 +409,7 @@ const players = {
       avatar: "/img/avatar/marriage/Rostyk_gej71.webp",
     },
   ],
-  "ZoomSos123": [
-    {
-      role: "Мер",
-      telegram: "t.me/kitsuxen",
-      category: "Мерія",
-      avatar: "https://tr.rbxcdn.com/30DAY-AvatarHeadshot-5CD467A78208C95DC904F3F5209EE15C-Png/150/150/AvatarHeadshot/Png/noFilter",
-    },
-  ],
-  "master_patata1": [
-    {
-      "role": "Секретар",
-      "telegram": "t.me/hello_amqruko",
-      "category": "Мерія",
-      "avatar": "https://i.postimg.cc/8C40Xrbd/IMG-20260624-004700-310.jpg",
-    },
-  ],
-  "Den45bgg": [
-    {
-      "role": "Охоронець",
-      "telegram": "t.me/Hoslifx7pro6ka",
-      "category": "Мерія",
-      "avatar": "https://i.postimg.cc/Ls3cdWmK/IMG-20260624-004339-483.jpg",
-    },
-  ],
-  "34436euf": [
-    {
-      "role": "Охоронець",
-      "telegram": "t.me/Artem75fgj",
-      "category": "Мерія",
-      "avatar": "https://i.postimg.cc/ydB7c98P/IMG-20260624-003930-118.jpg",
-    },
-  ],
-  "makspsava": [
-    {
-      "role": "Охоронець",
-      "telegram": "t.me/RoksyxMaks",
-      "category": "Мерія",
-      "avatar": "https://i.postimg.cc/qRC2hPgC/IMG-20260624-003623-734.jpg",
-    },
-  ],
-};
 
-
+// мерія
 
 
