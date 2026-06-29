0dd9c44456df13014346364425c9fe8e42e833e3
EzDevPy <samtarpro@gmail.com>
2026-06-29 18:33:57 +0300

    add weapon/val1n0tm

--- licenses.js
+++ licenses.js
@@ -40,6 +40,7 @@ window.licenses = {
     "34436euf": { status: "Дійсна", expiry: "24.07.2026", telegram: "t.me/Artem75fgj" },
 "Asda7771jog": { status: "Дійсна", expiry: "24.07.2026", telegram: "t.me/burmalda1488676752" },
     "Mativiti_aaa": { status: "Дійсна", expiry: "24.07.2026", telegram: "t.me/Matvei_Lego0_0" },
+    "val1n0tm": { cans: "CANS через val1n0tm.", status: "Дійсна", expiry: "24.07.2026", telegram: "t.me/g0gocat" },
 },
 
   // Зареєстровані бізнеси
