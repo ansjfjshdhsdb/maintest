c8db03d22a97bd25522b63adaf8ef343c2d542bc
EzDevPy <samtarpro@gmail.com>
2026-07-01 23:57:39 +0300

    add weapon/SIGMA2019827

--- licenses.js
+++ licenses.js
@@ -44,6 +44,7 @@ window.licenses = {
     "val1n0tm": { cans: "CANS через val1n0tm.", status: "Дійсна", expiry: "24.07.2026", telegram: "t.me/g0gocat" },
     "Asda7771jog": { status: "Дійсна", expiry: "24.07.2026", telegram: "t.me/burmalda1488676752" },
     "Slammed310": { status: "Дійсна", expiry: "02.08.2026", telegram: "6026275163" },
+    "SIGMA2019827": { status: "Дійсна", expiry: "02.08.2026", telegram: "8378012679" },
 },
 
   // Зареєстровані бізнеси
