e94c9a5db46baa63bce14c767ca2bd5a16f20bbb
EzDevPy <samtarpro@gmail.com>
2026-07-03 18:46:11 +0300

    add weapon/mativiti_aaa

--- licenses.js
+++ licenses.js
@@ -50,6 +50,7 @@ window.licenses = {
 "makspsava": { status: "Дійсна", expiry: "24.07.2026", telegram: "t.me/RoksyxMaks" },
     "34436euf": { status: "Дійсна", expiry: "24.08.2026", telegram: "t.me/Artem75fgj" },
     "Val1n0tm": { status: "Дійсна", expiry: "24.08.2026", telegram: "t.me/g0gocat" },
+    "mativiti_aaa": { status: "Дійсна", expiry: "24.08.2026", telegram: "t.me/Matvei_Lego0_0" },
 },
 
   // Зареєстровані бізнеси
