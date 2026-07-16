4c13c4f3991d8f3bba7e2c5c6c88303dc2071b0b
EzDevPy <samtarpro@gmail.com>
2026-07-16 22:24:18 +0300

    add weapon/CAXAPOK_QUW

--- licenses.js
+++ licenses.js
@@ -60,6 +60,7 @@ window.licenses = {
     "Caston08963": { status: "Дійсна", expiry: "15.08.2026", telegram: "7557434574" },
     "3cy_77781046": { status: "Дійсна", expiry: "15.08.2026", telegram: "6370149052" },
     "Zaj_Zuga3": { status: "Дійсна", expiry: "15.02.2026", telegram: "6025730893" },
+    "CAXAPOK_QUW": { status: "Дійсна", expiry: "15.08.2026", telegram: "6025730893" },
 },
 
   // Зареєстровані бізнеси
