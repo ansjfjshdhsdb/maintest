561d438c676c1e2dd00f9c0fecdc43f2411c9c0b
EzDevPy <samtarpro@gmail.com>
2026-07-17 00:03:47 +0300

    add police/Ya_amiD

--- licenses.js
+++ licenses.js
@@ -781,6 +781,7 @@ window.licenses = {
     "YTNEON": { role: "1", status: "835575", expiry: ".", telegram: "8357562286" },
     "Yan_Salii": { role: "1", status: "629482", expiry: "«»", telegram: "6299209889" },
     "Yrik_232345": { role: "1", status: "663768", expiry: "«»", telegram: "6635850024" },
+    "Ya_amiD": { role: "1", status: "501472", expiry: "«»", telegram: "5013338452" },
     // Z
     "zer14685": { "role": "8 700", "status": "607719", "expiry": "", "telegram": "6078536424" },
     "Zoomsos123": { "role": "65 129", "status": "350533", "expiry": "", "telegram": "5339606531" },
