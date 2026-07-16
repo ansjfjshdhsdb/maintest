b3603adcd19eb4ca001058a98666147f9d956f1b
EzDevPy <samtarpro@gmail.com>
2026-07-16 22:20:13 +0300

    add dbr/Zaj_Zuga3

--- licenses.js
+++ licenses.js
@@ -830,6 +830,7 @@ window.licenses = {
     "Zaj_Zuga3": { role: "Капрал ДБР", status: "105602", telegram: "6025730893" },
     "CAXAPOK_QUW": { role: "Капрал ДБР", status: "762117", telegram: "1172225259" },
     "BMW_M5F456": { role: "Капрал ДБР", status: "898806", telegram: "8064092076" },
+    "Zaj_Zuga3": { role: "Капрал ДБР", status: "105602", telegram: "6025730893" },
 },
 
   // Таксистська ліцензія
